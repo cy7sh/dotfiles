069758bdf3f3ee1764b19da4d2e149dddcb97844
Cy Pokhrel <cy@cy7.sh>
2024-10-16 21:38:32 -0400

    nvim-cmp bindings change

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -1,22 +1,36 @@
 local cmp = require("cmp")
 
-cmp.setup({
+cmp.setup {
   snippet = {
     expand = function(args)
       vim.fn["UltiSnips#Anon"](args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert({
+  mapping = cmp.mapping.preset.insert {
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  sources = cmp.config.sources({
+  },
+  sources = cmp.config.sources {
     { name = "nvim_lsp" },
     { name = "ultisnips" },
     { name = "buffer" },
     { name = "path" },
-  })
-})
+  }
+}
